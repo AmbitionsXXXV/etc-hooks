34d11f03208559da03200b1286097a4eea003798
etctera <yilonglei790@gmail.com>
2023-09-23 22:05:05 +0800

    feat: delete .idea & .umi

--- .umi/dumi/layout.tsx
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import React from 'react';
-import config from '@@/dumi/config';
-import demos from '@@/dumi/demos';
-import apis from '@@/dumi/apis';
-import Layout from '/Users/jiantianjianghui/Web_Project/etc-hooks/node_modules/.pnpm/@umijs+preset-dumi@1.1.53_react-dom@18.2.0_react-router@6.16.0_react@18.2.0_typescript@5.2.2_umi@3.5.41/node_modules/@umijs/preset-dumi/lib/theme/layout';
-
-export default (props) => <Layout {...props} config={config} demos={demos} apis={apis} />;
