9d2fcf1556c5084f972da002f43b8752e1d1690b
etctera <yilonglei790@gmail.com>
2023-09-24 21:33:12 +0800

    feat: webpack config

---
+++ packages/hooks/webpack.config.js
@@ -0,0 +1,12 @@
+const merge = require('webpack-merge')
+const common = require('../../webpack.common')
+const path = require('path').resolve(__dirname, './dist')
+
+module.exports = merge(common, {
+  entry: './es/index.js',
+  output: {
+    filename: 'etc-hooks.js',
+    library: 'etc-hooks',
+    path,
+  },
+})
