e540a062a9db67a125dcb8621833316fec7a9663
etctera <yilonglei790@gmail.com>
2023-09-24 22:34:01 +0800

    feat: add jest config

---
+++ jest-setup.js
@@ -0,0 +1,13 @@
+const screenfullMethods = [
+  'requestFullscreen',
+  'exitFullscreen',
+  'fullscreenElement',
+  'fullscreenEnabled',
+  'fullscreenchange',
+  'fullscreenerror',
+]
+
+screenfullMethods.forEach((item) => {
+  document[item] = () => {}
+  HTMLElement.prototype[item] = () => {}
+})
