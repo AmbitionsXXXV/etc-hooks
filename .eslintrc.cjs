9b67541b383c4df15a4aad0654c8633ed10400cc
etctera <yilonglei790@gmail.com>
2023-09-27 10:17:18 +0800

    fix: eslint extents path

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,1 +1,1 @@
-module.exports = { extends: ['etc-fe-eslint-config/typescript/react', 'prettier'] }
+module.exports = { extends: ['./node_modules/etc-fe-eslint-config/typescript/react', 'prettier'] }
